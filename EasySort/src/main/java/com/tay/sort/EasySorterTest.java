bcb9a7bb34bd1243bd8892606d0d780d36873e6e
tangaiyun@shein.com <tangaiyun@shein.com>
2019-09-20 09:23:13 +0800

    refactor the package

---
+++ EasySort/src/main/java/com/tay/sort/EasySorterTest.java
@@ -0,0 +1,59 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * @author  Aiyun Tang
+ * @mail aiyun.tang@gmail.com
+ */
+package com.tay.sort;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+public class EasySorterTest {
+	@Data
+	@AllArgsConstructor
+	static class MyBean {
+		private String name;
+		private Integer score;
+		private Integer index;
+	}
+
+	public static void main(String[] args) {
+		MyBean a = new MyBean("A", 98, 3);
+		MyBean b = new MyBean("B", 67, 5);
+		MyBean c = new MyBean("C", null, 4);
+		MyBean d = new MyBean("D", 89, 1);
+		MyBean e = new MyBean("E", 98, null);
+		MyBean f = new MyBean("F", 89, 6);
+		MyBean g = new MyBean("G", 98, 3);
+		List<MyBean> list = new ArrayList<>();
+
+		list.add(a);
+		list.add(b);
+		list.add(c);
+		list.add(d);
+		list.add(e);
+		list.add(f);
+		list.add(g);
+	
+		List<MyBean> result = EasySorter.sort(list, "-score,index,-name", false);
+
+		result.forEach(System.out::println);
+
+	}
+}
