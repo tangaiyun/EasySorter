df567c38dcc920d3aff145bbc184992078d58532
Henry Tang <15520929@qq.com>
2024-04-02 11:30:08 +0800

    Update SortRule.java

--- EasySort/src/main/java/com/tay/sort/SortRule.java
+++ EasySort/src/main/java/com/tay/sort/SortRule.java
@@ -14,16 +14,15 @@ public class SortRule<T> {
     private Function<T, Comparable> keyExtractor; // 键提取器，用于从元素中提取比较键
     @Getter
     private boolean isDesc; // 排序方向标志，true为降序，false为升序
+    @Getter
+    private boolean isNullFirst; // 指示null值是排在前面
 
-    /**
-     * 构造函数。
-     *
-     * @param keyExtractor 键提取器，从T类型元素中提取出可比较的键
-     * @param isDesc       是否降序排序，true表示降序，false表示升序
-     */
-    public SortRule(Function<T, Comparable> keyExtractor, boolean isDesc) {
+
+    // 构造函数也需要添加相应的参数
+    public SortRule(Function<T, Comparable> keyExtractor, boolean isDesc, boolean isNullFirst) {
         this.keyExtractor = keyExtractor;
         this.isDesc = isDesc;
+        this.isNullFirst = isNullFirst;
     }
 }
 
